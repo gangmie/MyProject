9bf8e12634bfbed5a15ca06206531f0a400ffeb7
gangmie <150205595+gangmie@users.noreply.github.com>
2025-10-01 14:30:57 +0900

    Update Memo.java

--- DCU-Memo/src/main/java/com/example/board/entity/Memo.java
+++ DCU-Memo/src/main/java/com/example/board/entity/Memo.java
@@ -17,18 +17,16 @@ public class Memo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // ✅ DB 컬럼은 member_email, Java에서는 memberEmail
     @Column(name = "member_email", nullable = false)
     private String memberEmail;
 
-    // ✅ DB 컬럼은 is_public, Java에서는 isPublic
     @Column(name = "is_public", nullable = false)
     private Boolean isPublic;
 
     @Column(name = "mod_date")
     private LocalDateTime modDate;
 
-    private String name;   // 작성자 이름
+    private String name;   // 작성자 닉네임
     private String title;  // 메모 제목
 
     @Column(columnDefinition = "TEXT")
